22a8e816a9b7a8bab3d466db3057fda1602f5824
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2020-05-15 13:46:14 +0600

    Creating DP and reorder Algo

---
+++ Algorithm/Dynamic Programming/02.StaircaseProblemRecursion.cpp
@@ -0,0 +1,114 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll          long long
+#define pb          push_back
+#define mp          make_pair
+#define ff          first
+#define ss          second
+#define vi          vector<int>
+#define si          set<int>
+#define pi          pair<int,int>
+#define FOR(i,n)    for(int i=0;i<n;i++)
+#define fast        ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define SP(x)       fixed<<setprecision(x)
+#define fWrite      freopen("out.txt", "w", stdout);//to write the output in a txt file
+#define fRead       freopen("in.txt", "r", stdin);
+#define asi         cout<<"Check"<<endl
+#define bye         return 0
+#define ok          cout<<"OK"<<endl
+#define NL          "\n"
+#define mod         1000000007
+//lower_bound == Shoman othoba prothom boro element ta return korbe//iterator return kore
+//upper bound mane first boro element return korbe
+//string s(n,'a');
+//ans+=string(r,'R');
+
+ll Digits(ll a)
+{
+    return(floor(log10(a))+1);
+}
+ll BMod(ll B,ll P,ll M)
+{
+    ll R=1;
+    B=B%M;
+    while(P)
+    {
+        if(P&1)
+            R=(R*B)%M;
+        P >>= 1;
+        B=(B*B)%M;
+    }
+    return R;
+}
+ll POW(ll  B,ll P)
+{
+    ll R=1;
+    while(P)
+    {
+        if(P&1)
+            R=R*B;
+        B=B*B;
+        P>>=1;
+    }
+    return R;
+}
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+ll lcm(ll a,ll b)
+{
+    return a*b/gcd(a,b);
+
+}
+
+//problem link
+//https://www.codechef.com/problems/JAM11
+
+
+ll dp[100005];
+
+
+ll stair(ll n)
+{
+
+    if(n==1)
+        return 1;
+    else if(n==2)
+        return 2;
+    else if(n==3)
+        return 4;
+
+    else
+    {
+        if(dp[n]!= -1)
+            return dp[n];
+        else
+        {
+            dp[n]= ( stair(n-1)+stair(n-2)+stair(n-3) )%mod;
+            return dp[n];
+        }
+    }
+}
+
+
+int main()
+{
+    fast
+    ll t;
+    cin>>t;
+     memset(dp,-1,sizeof(dp));
+    //stair(10000);
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        cout<<stair(n)<<NL;
+    }
+
+}
+
+
