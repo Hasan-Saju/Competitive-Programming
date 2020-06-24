72c1874041a7639228addf708b78ba3daf975833
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2020-06-24 21:33:30 +0600

    update

---
+++ Codeforces/236B.cpp
@@ -0,0 +1,137 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define ull         unsigned long long
+#define pb          push_back
+#define mp          make_pair
+#define ff          first
+#define ss          second
+#define vi          vector<int>
+#define si          set<int>
+#define pi          pair<int,int>
+#define FOR(i,n)    for(int i=0;i<n;i++)
+
+#define min3(a,b,c)         min(a,min(b,c))
+#define max3(a,b,c)         max(a,max(b,c))
+#define min4(a,b,c,d)       min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)       max(a,max(b,max(c,d)))
+#define max5(a,b,c,d,e)     max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e)     min(min3(a,b,c),min(d,e))
+
+#define fast        ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define SP(x)       fixed<<setprecision(x)
+#define fWrite      freopen("out.txt", "w", stdout);//to write the output in a txt file
+#define fRead       freopen("in.txt", "r", stdin);
+#define PI          2*acos(0.0)
+#define asi         cout<<"Check"<<endl
+#define bye         return 0
+#define ok          cout<<"OK"<<endl
+#define NL          "\n"
+
+//lower_bound == Shoman othoba prothom boro element ta return korbe//iterator return kore
+//upper bound mane first boro element return korbe
+//string s(n,'a');
+//ans+=string(r,'R');
+
+ll Digits(ll a)
+{
+    return(floor(log10(a))+1);
+}
+ll BMod(ll B,ll P,ll M)
+{
+    ll R=1;
+    B=B%M;
+    while(P)
+    {
+        if(P&1)
+            R=(R*B)%M;
+        P >>= 1;
+        B=(B*B)%M;
+    }
+    return R;
+}
+ll POW(ll  B,ll P)
+{
+    ll R=1;
+    while(P)
+    {
+        if(P&1)
+            R=R*B;
+        B=B*B;
+        P>>=1;
+    }
+    return R;
+}
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+ll lcm(ll a,ll b)
+{
+    return a*b/gcd(a,b);
+
+}
+
+ll store[1000005];
+ll mod=1073741824;
+
+
+ll numDiv(ll x)
+{
+   // cout<<"in";
+    ll d=0;
+
+    for(int i=1; i<=sqrt(x); i++)
+    {
+        if(x%i==0 and x%(x/i)==0 and i!=(x/i))
+        {
+            d+=2;
+            // cout<<"fn1 "<<i<<NL;
+        }
+
+        else if(x%i==0 and x%(x/i)==0 and i==(x/i))
+        {
+            d++;
+            //cout<<"fn2 "<<i<<NL;
+        }
+    }
+    return d;
+}
+
+int main()
+{
+    fast
+    ll a,b,c;
+    cin>>a>>b>>c;
+
+    ll ans=0;
+
+    for(int i=1; i<=a; i++)
+    {
+        for(int j=1; j<=b; j++)
+        {
+            for(int k=1; k<=c; k++)
+            {
+                if(store[i*j*k]==0)
+                {
+                    ll x= numDiv(i*j*k);
+                    ans= (ans%mod) + (x%mod);
+                    store[i*j*k]= x%mod;
+                    //cout<<x<<" "<<i*j*k<<NL;
+                }
+                else
+                {
+                    ans= (ans%mod) + (store[i*j*k]%mod);
+                    //cout<<store[i*j*k]<<" "<<i*j*k<<NL;
+                }
+            }
+        }
+    }
+
+    cout<<ans;
+}
+
---
+++ Codeforces/876B.cpp
@@ -0,0 +1,119 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define ull         unsigned long long
+#define pb          push_back
+#define mp          make_pair
+#define ff          first
+#define ss          second
+#define vi          vector<int>
+#define si          set<int>
+#define pi          pair<int,int>
+#define FOR(i,n)    for(int i=0;i<n;i++)
+
+#define min3(a,b,c)         min(a,min(b,c))
+#define max3(a,b,c)         max(a,max(b,c))
+#define min4(a,b,c,d)       min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)       max(a,max(b,max(c,d)))
+#define max5(a,b,c,d,e)     max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e)     min(min3(a,b,c),min(d,e))
+
+#define fast        ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define SP(x)       fixed<<setprecision(x)
+#define fWrite      freopen("out.txt", "w", stdout);//to write the output in a txt file
+#define fRead       freopen("in.txt", "r", stdin);
+#define PI          2*acos(0.0)
+#define asi         cout<<"Check"<<endl
+#define bye         return 0
+#define ok          cout<<"OK"<<endl
+#define NL          "\n"
+
+//lower_bound == Shoman othoba prothom boro element ta return korbe//iterator return kore
+//upper bound mane first boro element return korbe
+//string s(n,'a');
+//ans+=string(r,'R');
+
+ll Digits(ll a)
+{
+    return(floor(log10(a))+1);
+}
+ll BMod(ll B,ll P,ll M)
+{
+    ll R=1;
+    B=B%M;
+    while(P)
+    {
+        if(P&1)
+            R=(R*B)%M;
+        P >>= 1;
+        B=(B*B)%M;
+    }
+    return R;
+}
+ll POW(ll  B,ll P)
+{
+    ll R=1;
+    while(P)
+    {
+        if(P&1)
+            R=R*B;
+        B=B*B;
+        P>>=1;
+    }
+    return R;
+}
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+ll lcm(ll a,ll b)
+{
+    return a*b/gcd(a,b);
+
+}
+
+int ind[100005];
+
+int main()
+{
+    fast
+    int n,k,m,flag=0;
+    cin>>n>>k>>m;
+
+    int win=0;
+    vi v;
+
+    FOR(i,n)
+    {
+        int x;
+        cin>>x;
+        v.pb(x);
+        ind[x%m]++;
+        if(ind[x%m]>=k)
+            flag=1,win=x%m;
+    }
+    if(!flag)
+        cout<<"No"<<NL;
+    else
+    {
+        cout<<"Yes"<<NL;
+        int bla=0;
+        for(auto x:v )
+        {
+            if(x%m==win)
+            {
+                cout<<x<<" ";
+                bla++;
+                if(bla==k)
+                    break;
+            }
+
+
+        }
+    }
+}
+
