fdca584284c493b2b1f3df79c042ae0160ef775d
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2020-12-20 12:37:36 +0600

    #maxflow

--- LightOj/1155.cpp
+++ LightOj/1155.cpp
@@ -1,37 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define INF INT_MAX
+
 int n;
-vector<vector<int> >capacity(200,vector<int>(200,0));
-vector<int>adj[200];
-int val[200];
+vector<vector<int> >capacity(500,vector<int>(500,0));
+vector<int>adj[500];
+int val[500];
 
 int bfs(int s, int t, vector<int>& parent)
 {
-    cout<<"hello bfs"<<endl;
-    //fill(parent.begin(), parent.end(), -1);
-    for(int p=0; p<=200; p++)
-        parent[p]=-1;
-
+    fill(parent.begin(), parent.end(), -1);
     parent[s] = -2;
     queue<pair<int, int>> q;
-    q.push({s, INT_MAX});
+    q.push({s, INF});
 
     while (!q.empty())
     {
         int cur = q.front().first;
         int flow = q.front().second;
         q.pop();
-        cout<<"bfs "<<cur<<endl;
 
         for (int next : adj[cur])
         {
-             cout<<" next "<<next<<" param "<<parent[next]<<" "<<capacity[cur][next]<<" "<<val[next]<<endl;
-            if (parent[next] == -1 && capacity[cur][next]>0 && val[next]>0 )
+            if (parent[next] == -1 && capacity[cur][next])// and val[next]>0)
             {
                 parent[next] = cur;
-                int new_flow = min( min(flow, capacity[cur][next]), val[next]);
-                cout<<next<<" bfs er ekdom vitor "<<new_flow<<endl;
+                int new_flow = min(flow, capacity[cur][next]);
+               // new_flow=min(new_flow,val[next]);
+
                 if (next == t)
                     return new_flow;
                 q.push({next, new_flow});
@@ -45,29 +42,27 @@ int bfs(int s, int t, vector<int>& parent)
 int maxflow(int s, int t)
 {
     int flow = 0;
-    vector<int> parent(n+2);
+    vector<int> parent(500);
     int new_flow;
-   // cout<<"in\n";
-    //cout<<bfs(s,t,parent)<<"\n";
 
     while (new_flow = bfs(s, t, parent))
     {
         flow += new_flow;
-        cout<<flow<<" flow "<<new_flow<<"\n";
-
         int cur = t;
+        //cout<<"flow: "<<new_flow<<"\n";
         while (cur != s)
         {
             int prev = parent[cur];
             capacity[prev][cur] -= new_flow;
-
-            //val[prev]-=new_flow;
-            val[cur]-=new_flow;
-
             capacity[cur][prev] += new_flow;
-            cout<<prev<<" -> "<<cur<<"\n";
+
+            //val[cur]-=new_flow;
+
+            //cout<<prev<<" -> "<<cur<<endl;
+
             cur = prev;
         }
+        //cout<<"\n\n\n";
     }
 
     return flow;
@@ -76,82 +71,77 @@ int maxflow(int s, int t)
 int main()
 {
 
-    int test;
-    cin>>test;
+    int t;
+    cin>>t;
 
-
-    for(int i=1; i<=test; i++)
+    for(int a=1; a<=t; a++)
     {
-        int n;
         cin>>n;
-        memset(val,0,sizeof(val));
 
-        val[0]=INT_MAX;
-        val[n+1]=INT_MAX;
+        int x;
+        for(int b=0; b<200; b++)
+            val[b]=INT_MAX;
 
-        for(int j=1; j<=n; j++)
-        {
-            int x;
-            cin>>x;
-            val[j]=x;
-        }
+        for(int b=1; b<=n; b++)
+            {
+                //cin>>val[b];
+                cin>>x;
+                capacity[b][200+b]=x;
+            }
+
 
         int edge;
         cin>>edge;
 
-        for(int j=0; j<edge; j++)
+        for(int i=0; i<edge; i++)
         {
             int a,b,cost;
             cin>>a>>b>>cost;
 
+            adj[a].push_back(200+a);
+            adj[200+a].push_back(a);
 
-            adj[a].push_back(b);
-            adj[b].push_back(a);
-            capacity[a][b]+=cost;
+            adj[200+a].push_back(b);
+            adj[b].push_back(200+a);
 
+            adj[b].push_back(200+b);
+            adj[200+b].push_back(b);
+
+
+            capacity[200+a][b]+=cost;
         }
+
         int monu,dhaka;
         cin>>monu>>dhaka;
 
-
-        for(int k=0; k<monu; k++)
+        for(int b=1; b<=monu; b++)
         {
-            int x;
             cin>>x;
             adj[0].push_back(x);
             adj[x].push_back(0);
 
-            capacity[0][x]=INT_MAX;
+            capacity[0][x]+=INT_MAX;
         }
-        for(int k=0; k<dhaka; k++)
+
+        for(int b=1; b<=dhaka; b++)
         {
-            int x;
             cin>>x;
-            adj[x].push_back(n+1);
-            adj[n+1].push_back(x);
-            capacity[x][n+1]=INT_MAX;
+            adj[200+x].push_back(n+1);
+            adj[n+1].push_back(200+x);
+
+            capacity[200+x][n+1]=INT_MAX;
         }
 
-//        cout<<"matrix\n";
-//        for(int x=0; x<=n+1; x++)
-//        {
-//            for(int z=0; z<=n+1; z++)
-//                cout<<capacity[x][z]<<" ";
-//            cout<<endl;
-//        }
 
-
-
-        cout<<"Case "<<i<<": "<<maxflow(0,n)<<"\n";
-
-        for(int j=0; j<200; j++)
+        cout<<"Case "<<a<<": "<<maxflow(0,n+1)<<"\n";
+        //clearing vectors
+        for(int j=0; j<500; j++)
         {
             adj[j].clear();
         }
-        capacity = vector<vector<int> > (200, vector<int>(200, 0));
+        capacity = vector<vector<int> > (500, vector<int>(500, 0));
     }
 
 
 
 }
-
