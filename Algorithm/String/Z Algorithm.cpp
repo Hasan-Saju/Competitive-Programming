2cda50be7450c32fe3f94b0922f554871e33eca8
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2020-12-27 20:59:38 +0600

    z algo added

---
+++ Algorithm/String/Z Algorithm.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string str;
+int z[200005];
+
+void calc_z()
+{
+    int L = 0;
+    int R = 0;
+    int i = 0;
+    z[0] = 0;
+
+    for(int i=1;i<str.size();i++)
+    {
+        if(i>R)
+        {
+            L = i;
+            R = i;
+            int in = 0;
+            while(str[R] == str[in] && R<str.size()) R++,in++;
+            z[i] = R - L;
+            R--;
+        }
+        else
+        {
+            int k = i - L;
+            if(z[k]< R - i + 1) z[i] = z[k];
+            else
+            {
+                L = i;
+                int in = 0;
+                while(str[R]==str[in] && R<str.size())R++,in++;
+                z[i] = R - L;
+                R--;
+            }
+        }
+    }
+}
+int main()
+{
+    string pat,text;
+    cin>>pat>>text;
+
+    str = pat + "$" + text;
+    calc_z();
+    for(int i=0;i<str.size();i++)cout<<z[i]<<" ";
+}
