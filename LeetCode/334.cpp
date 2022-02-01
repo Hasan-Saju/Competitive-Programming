7c7e7a3a1244b9c0dbd7e34f429f5da4ebe0cd29
Hasan Saju <hasansaju11@gmail.com>
2022-02-01 23:06:05 +0600

    LeetCode Day 15

--- LeetCode/334.cpp
+++ LeetCode/334.cpp
@@ -1,22 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool increasingTriplet(vector<int>& nums) {
-        long long mn=LONG_MIN;
-        for(int i=0;i<nums.size();i++)
+    bool increasingTriplet(vector<int>& nums)
+    {
+        int first=INT_MAX,second=INT_MAX;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++)
         {
-            if()
+            if(second<nums[i])return true;
+            if(first<nums[i])second=min(second,nums[i]);
+            first=min(first,nums[i]);
         }
+        return false;
     }
 };
 
 int main()
 {
     Solution obj;
-    vector<int>vec = {2,7,11,15};
-    obj.increasingTriplet(vec);
+    vector<int>vec = {1,5,0,4,1,3};
+    bool ans=obj.increasingTriplet(vec);
+    cout<<ans;
 }
 
 
