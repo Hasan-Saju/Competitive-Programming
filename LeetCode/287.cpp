893c2fb40155884d2504f7381e6a843305df04e5
Hasan Saju <hasansaju11@gmail.com>
2022-02-01 23:30:20 +0600

    test past commit commit back date

---
+++ LeetCode/287.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+
+    }
+};
+int main()
+{
+    Solution obj;
+    vector<int>vec = {1,3,4,2,2};
+    int ans=obj.findDuplicate(vec);
+    cout<<ans;
+}
+
+
+
+
+
+
