1774e4376f38d1060b053442874d15aaf8d4c53f
Hasan Saju <hasansaju11@gmail.com>
2024-11-26 23:30:43 -0500

    solved

--- LeetCode/994.cpp
+++ LeetCode/994.cpp
@@ -1,24 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
     int visited[15][15];
     int distance[15][15];
+    queue<int>q;
 public:
-    int bfs(int x, int y, vector<vector<int>>& grid)
+    int bfs(vector<vector<int>>& grid)
     {
-        cout<<"\nbfs\n";
-        cout<<x<<" "<<y<<"\n\n";
-        queue<int>q;
-        visited[x][y]=1;
-        distance[x][y] = 0;
-        q.push(x);
-        q.push(y);
+        memset(visited, 0, sizeof(visited));
+        memset(distance, 0, sizeof(distance));
 
         int depth = 0;
 
         int row = grid.size();
         int col = grid[0].size();
-        cout<<row<<" "<<col<<" row col \n";
 
         int X[4] = {0, 0, 1, -1};
         int Y[4] = {1, -1, 0, 0};
@@ -29,29 +25,28 @@ public:
             q.pop();
             int uy = q.front();
             q.pop();
+            visited[ux][uy] = 1;
 
-            for(int i=0;i<4;i++)
+            for(int i=0; i<4; i++)
             {
                 int nx = ux+X[i];
                 int ny = uy+Y[i];
-                cout<<nx<<"  out bfs "<<ny<<" "<<!visited[nx][ny]<<" "<< grid[nx][ny]<<"\n";
+
                 if(nx>=0 and ny>=0 and nx<row and ny<col and visited[nx][ny]==0 and grid[nx][ny]==1)
                 {
-                    cout<<nx<<" in bfs "<<ny<<"\n";
                     visited[nx][ny] = 1;
                     distance[nx][ny] = distance[ux][uy] + 1;
-                    cout<<distance[nx][ny]<<"\n";
+
                     q.push(nx);
                     q.push(ny);
-
                     depth = max(depth, distance[nx][ny]);
                 }
             }
-            cout<<" sz "<<q.size()<<"\n";
         }
         return depth;
     }
-    int orangesRotting(vector<vector<int>>& grid) {
+    int orangesRotting(vector<vector<int>>& grid)
+    {
         int row = grid.size();
         int col = grid[0].size();
         int ans = 0;
@@ -60,10 +55,14 @@ public:
             for(int j=0; j<col; j++)
             {
                 if(grid[i][j]==2)
-                    ans = max(ans, bfs(i,j,grid));
+                {
+                    q.push(i);
+                    q.push(j);
+                }
             }
         }
-
+        if(q.size())
+            ans = bfs(grid);
         for(int i=0; i<row; i++)
         {
             for(int j=0; j<col; j++)
