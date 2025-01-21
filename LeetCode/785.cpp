080b4e16f46444735c3088700918dab02d7e8637
Hasan Saju <hasansaju11@gmail.com>
2025-01-20 21:22:17 -0500

    Updated

---
+++ LeetCode/785.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void graphColoringBFS(int x, int y)
+    {
+
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+
+    }
+};
+int main()
+{
+    Solution obj;
+    vector<vector<int>>vec = {{1,2,3},{0,2},{0,1,3},{0,2}};
+    obj.isBipartite(vec);
+}
