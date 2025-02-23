4928ad819521c348ef16cfc25274f864a2812d0e
Hasan Saju <hasansaju11@gmail.com>
2025-02-22 23:32:00 -0500

    update

---
+++ LeetCode/680.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool validPalindrome(string s) {
+
+    }
+};
+
+int main()
+{
+    Solution obj;
+    vector<int>vec = {2,7,11,15};
+    obj.twoSum(vec);
+}
+
